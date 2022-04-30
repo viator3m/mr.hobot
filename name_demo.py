f6f11a4995b66b25319c1f7aff0e23cb1a056be2
viator3m <viator3m@gmail.com>
2022-04-30 22:01:55 +0300

    first commit

--- name_demo.py
+++
@@ -1,7 +0,0 @@
-def simple(a, b):
-    return a + b
-
-
-if __name__ == '__main__':
-    print(simple(2, 2))
-    print(__name__)
--- name_demo2.py
+++
@@ -1,4 +0,0 @@
-from name_demo import simple
-
-print(simple(2, 2))
-print(__name__)
